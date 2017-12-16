b33e2d6da2027d2d0faacbbe8f04489ad9c10204
alex hayes <alexpghayes@gmail.com>
2017-12-16 14:36:44 -0800

    ignore warnings that aren't breaking things

--- R/hayeslib-package.r
+++ R/hayeslib-package.r
@@ -1,6 +1,7 @@
 #' hayeslib: a package to learn about packages. #meta
 #'
-#' Once the data manipulation functions get cleaned up there'll be a little explanation of what's available here.
+#' Once the data manipulation functions get cleaned up there'll be a
+#'   little explanation of what's available here.
 #'
 #' @docType package
 #' @name hayeslib
